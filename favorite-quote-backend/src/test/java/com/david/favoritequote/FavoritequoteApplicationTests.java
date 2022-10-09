8d78d627027b6c3f417305b5e8c3e9dde833fdc3
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2022-10-08 23:11:47 -0700

    initial commit for frontend

---
+++ favorite-quote-backend/src/test/java/com/david/favoritequote/FavoritequoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.favoritequote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoritequoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

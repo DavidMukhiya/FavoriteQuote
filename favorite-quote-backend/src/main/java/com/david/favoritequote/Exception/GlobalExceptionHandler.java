d06320b1a7de9033a14be0af485ba7b38659fddf
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2022-12-05 01:18:50 -0800

    handling the request with quote id not found

---
+++ favorite-quote-backend/src/main/java/com/david/favoritequote/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.david.favoritequote.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException e){
+        String message = e.getMessage();
+        ApiResponse apiResponse = new ApiResponse(message, false);
+        return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+}
